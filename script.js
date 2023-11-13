603f3fa7c7a5a2a2a06a9bd2870f7d24086f7ef8
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-13 19:20:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-//your JS code here. If required.
+const noOfDays = (year) => {
+  // write your code here
+
+  year = parseInt(year);
+
+  if (year < 1 && year > 9999) {
+    return "Please enter a valid year";
+  }
+
+  let isLeapYear;
+
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        isLeapYear = true;
+      } else {
+        isLeapYear = false;
+      }
+    } else {
+      isLeapYear = true;
+    }
+  } else {
+    isLeapYear = false;
+  }
+
+  if (isLeapYear) {
+    return 366;
+  } else {
+    return 365;
+  }
+};
+
+// Do not change the code below.
+const year = prompt("Enter the year - ");
+alert(noOfDays(year));
